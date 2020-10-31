566a3ae6a33a8f3e2b199257f0c7b8c1fb7b2320
Yash Khare <yashsja@gmail.com>
2020-10-31 20:17:33 +0530

    feat: Add all surveys

--- lib/design_course/models/category.dart
+++ lib/design_course/models/category.dart
@@ -26,4 +26,83 @@ class Category {
       title: 'Completed Inspections List',
     ),
   ];
+
+  static List<Category> surveys = <Category>[
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+      title: 'Tyres',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Steering',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Engine',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+      title: 'Suspension',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Horn',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+      title: 'Brake',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Lamps and Signals',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+      title: 'Embossing',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Speedometer',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+      title: 'Painting',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Wiper',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+      title: 'Body',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Electricals',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+      title: 'Finishing',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Road Test',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+        title: 'Pollution',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Safety Glasses',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace3.png',
+      title: 'Seat Belts',
+    ),
+    Category(
+      imagePath: 'assets/design_course/interFace4.png',
+      title: 'Emergency Information',
+    ),
+  ];
 }
